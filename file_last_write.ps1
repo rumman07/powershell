fef3cd07cb2555e77605ffd7df0fcd1843ca6cc9
rumman07 <rummanahmed.ra@gmail.com>
2017-06-29 04:49:04 -0500

    added provider and filesystem section

---
+++ file_last_write.ps1
@@ -0,0 +1,10 @@
+﻿Param(
+[Parameter(Mandatory=$true)]
+[string]$Path,
+
+[Parameter(Mandatory=$true)]
+[string]$LastWrite 
+) 
+
+GCI -Path $Path | Where-Object -FilterScript `
+ {($_.LastWriteTime -gt $LastWrite)}                
